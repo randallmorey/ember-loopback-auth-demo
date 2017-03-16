8acd0d0f76d4b44a94b2872ab6bd1e859b37da48
Randall Morey <randall@randallmorey.com>
2017-03-16 10:56:13 -0400

    simplified user form component and added docs

--- app/components/user/user-form/component.js
+++ app/components/user/user-form/component.js
@@ -1,10 +1,28 @@
 import Ember from 'ember';
 
+/**
+ * A simple user form component with fields for {@link module:app/user/model}.
+ *
+ * @module
+ * @augments ember-data/Model
+ * @augments ember-loopback-auth-demo/mixins/model-validator
+ * @example
+ *  {{user/user-form model=user save=(action externalSave)}}
+ */
 export default Ember.Component.extend({
-  user: null,
+  // =actions
+
+  /** @type {Object} */
   actions: {
-    saveUser() {
-      this.get('saveUser')(this.get('user'));
+    /**
+     * Trigger a bound `save` method when the form is submitted, passing the
+     * form's model instance.
+     *
+     * @function actions:submit
+     * @returns {undefined}
+     */
+    submit() {
+      this.get('save')(this.get('model'));
     }
   }
 });

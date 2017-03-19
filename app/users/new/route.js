e348e5e9175f3707e3038af05ea752dd3a21458a
Randall Morey <randall@randallmorey.com>
2017-03-19 14:13:58 -0400

    new user route should rollback/destroy new, unsaved records on willTransition action

--- app/users/new/route.js
+++ app/users/new/route.js
@@ -18,5 +18,27 @@ export default Ember.Route.extend({
    */
   model() {
     return this.store.createRecord('user');
+  },
+
+  // =actions
+
+  actions: {
+    /**
+     * Rollsback and removes the route's user model if it `isNew`.
+     *
+     * @function actions:willTransition
+     * @returns {undefined}
+     */
+    willTransition() {
+      const model = this.modelFor('users/new');
+      if (model.get('isNew')) {
+        // `rollbackAttributes` on a model that `isNew` both reverts all
+        // attributes to their defaults and causes the record to be removed
+        // from the store.
+        // Thus, this is a good way to clean up new unsaved records that we
+        // want to get rid of.
+        model.rollbackAttributes();
+      }
+    }
   }
 });
--- tests/unit/users/new/route-test.js
+++ tests/unit/users/new/route-test.js
@@ -21,3 +21,18 @@ test('it generates a new user instance', function (assert) {
     assert.equal(model.constructor.modelName, 'user');
   });
 });
+
+test('it should call rollbackAttributes on a new model on transition', function (assert) {
+  assert.expect(1);
+  let route = this.subject({
+    modelFor: function () {
+      return Ember.Object.create({
+        isNew: true,
+        rollbackAttributes() {
+          assert.ok(true, 'should call rollbackAttributes on a new model');
+        }
+      });
+    }
+  });
+  route.actions.willTransition.call(route);
+});

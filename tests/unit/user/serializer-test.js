c5597c5a0656929741aa4835ffca76b88b03714c
Randall Morey <randall@randallmorey.com>
2017-03-19 14:43:20 -0400

    simplified user serializer unit test and removed mock server dependency

--- tests/unit/user/serializer-test.js
+++ tests/unit/user/serializer-test.js
@@ -1,8 +1,4 @@
-import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
-import Pretender from 'pretender';
-
-const { run } = Ember;
 
 moduleForModel('user', 'Unit | Serializer | user', {
   // Specify the other units that are required for this test.
@@ -28,29 +24,23 @@ test('it serializes records', function (assert) {
   assert.equal(passwordConfirmation, undefined);
 });
 
-test('it normalizes records and nulls password, passwordConfirmation, and emailConfirmation', function (assert) {
-  let done = assert.async();
-  const server = new Pretender(function () {
-    this.post('/users', function (request) {
-      let response = JSON.parse(request.requestBody);
-      response.data.id = '1';
-      return [200, { 'Content-Type': 'application/json' }, JSON.stringify(response)];
-    });
-  });
-  run(() => {
-    const record = this.subject({
-      email: 'test@foo.com',
-      emailConfirmation: 'test@foo.com',
-      password: 'password1234',
-      passwordConfirmation: 'password1234'
-    });
-    record.save().then(() => {
-      assert.equal(record.get('email'), 'test@foo.com');
-      assert.equal(record.get('emailConfirmation'), null);
-      assert.equal(record.get('password'), null);
-      assert.equal(record.get('passwordConfirmation'), null);
-      server.shutdown();
-      done();
-    });
-  });
+test('it normalizes responses and nulls password, passwordConfirmation, and emailConfirmation', function (assert) {
+  const record = this.subject();
+  const serializer = this.store().serializerFor('user');
+  const responseHash = {
+    data: {
+      id: '1',
+      type: 'users',
+      attributes: {
+        email: 'test@foo.com',
+        emailConfirmation: 'test@foo.com',
+        password: 'password1234',
+        passwordConfirmation: 'password1234'
+      }
+    }
+  };
+  const normalizedHash = serializer.normalize(record.constructor, responseHash.data);
+  assert.equal(normalizedHash.data.attributes.password, null);
+  assert.equal(normalizedHash.data.attributes.passwordConfirmation, null);
+  assert.equal(normalizedHash.data.attributes.emailConfirmation, null);
 });

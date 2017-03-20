20a71777f6abadf6b9c4a6fdefd1e8e6cbddcae5
Randall Morey <randall@randallmorey.com>
2017-03-20 19:47:10 -0400

    override authenticator to accept a user model instance in authenticate method instead of ident and password strings

--- app/authenticators/application.js
+++ app/authenticators/application.js
@@ -1,5 +1,8 @@
+import Ember from 'ember';
 import Loopback from 'ember-simple-auth-loopback/authenticators/loopback';
 import config from '../config/environment';
+
+const { inject: { service } } = Ember;
 
 /**
  * Authenticates with a Loopback API server.
@@ -8,11 +11,43 @@ import config from '../config/environment';
  * @augments ember-simple-auth-loopback/authenticators/loopback
  */
 export default Loopback.extend({
+  // =dependencies
+
+  /** @type {Object} */
+  store: service(),
+
   // =attributes
 
   /**
    * @override
    * @type {String}
    */
-  loginEndpoint: config.api.host + '/' + config.api.namespace + '/users/login'
+  loginEndpoint: config.api.host + '/' + config.api.namespace + '/users/login',
+
+  /**
+   * Authenticates the session with credentials from the specified user instance
+   * by delegating to the `_super` method.  If authentication fails, a generic
+   * error message is applied to the user model instance's base field.
+   * It is recommended to use a disposable, unsaved user instance that is
+   * disposed of after successful authentication.
+   *
+   * @see module:ember-simple-auth-loopback/authenticators/loopback~authenticate
+   * @override
+   * @function
+   * @param {Object} user A user model instance
+   * @returns {Promise} A promise that when it resolves results in the session
+   *   becoming authenticated
+   */
+  authenticate(user, scope) {
+    const { email, password } = user.getProperties('email', 'password');
+    const promise = this._super(email, password, scope);
+    promise.catch(() => {
+      const recordForErrors = user.adapterDidInvalidate ?
+        user : user._internalModel;
+      this.get('store').recordWasInvalid(recordForErrors, {
+        base: ['login failed']
+      });
+    });
+    return promise;
+  }
 });
--- tests/unit/application/authenticator-test.js
+++ tests/unit/application/authenticator-test.js
@@ -1,9 +1,13 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 import config from '../../../config/environment';
 
+const { run } = Ember;
+const { RSVP: { Promise } } = Ember;
+
 moduleFor('authenticator:application', 'Unit | Authenticator | application', {
   // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
+  needs: ['model:user']
 });
 
 // Replace this with your real tests.
@@ -15,4 +19,41 @@ test('it exists', function (assert) {
 test('its `loginEndpoint` attribute is derived through app config', function (assert) {
   const authenticator = this.subject();
   assert.equal(authenticator.get('loginEndpoint'), config.api.host + '/' + config.api.namespace + '/users/login');
+});
+
+test('its `authenticate` method calls `makeRequest` with credentials from the passed user instance', function (assert) {
+  const done = assert.async();
+  const authenticator = this.subject({
+    makeRequest(url, credentials) {
+      assert.ok(true, 'makeRequest() should be called');
+      assert.equal(credentials.email, 'test@foo.com');
+      assert.equal(credentials.password, 'password1234');
+      done();
+    }
+  });
+  const store = authenticator.get('store');
+  run(() => {
+    const user = store.createRecord('user', {
+      email: 'test@foo.com',
+      password: 'password1234'
+    });
+    authenticator.authenticate(user);
+  });
+});
+
+test('its `authenticate` method adds an error to the base field of the passed user instance if authentication fails', function (assert) {
+  const done = assert.async();
+  const authenticator = this.subject({
+    makeRequest() {
+      return Promise.reject({responseJSON: '{}'});
+    }
+  });
+  const store = authenticator.get('store');
+  run(() => {
+    const user = store.createRecord('user');
+    authenticator.authenticate(user).catch(() => {
+      assert.ok(user.get('errors.base')[0].message);
+      done();
+    });
+  });
 });
